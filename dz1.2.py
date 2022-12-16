09c4ceec05f09741a8a7a8471b5b945fde371779
St-Katerina <stekacheva.katerina@yandex.ru>
2022-12-16 15:34:18 +0300

    ДЗ к первому уроку

---
+++ dz1.2.py
@@ -0,0 +1,7 @@
+# Задание №2
+
+times = int(input("Введите время в секундах: "))
+sec = times % 60
+hours = times // 3600
+minuts = (times // 60) % 60
+print("Время: {}:{}:{}" .format(hours, minuts, sec))
